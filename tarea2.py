a53d009d12cdb931e632e8405ddd8e6c05e95d85
MarilynBG <abravog1301@alumno.ipn.mx>
2022-07-04 23:07:18 -0500

    se agrega tarea 2

---
+++ tarea2.py
@@ -0,0 +1,11 @@
+
+n=int(input("Ingrese el n'umero de aisgnaturas:\n"))
+print("Ingrese las asignaturas:\n")
+asig=[]
+
+for i in range(n):
+    asig.append(input())
+print("\n")
+
+for i in asig:
+    print(i)
